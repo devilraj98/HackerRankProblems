f1025b62d99d651b014c2c161bb7873a55be9c03
[Neeraj kumar] <[neerajraj985275@gmail.com]>
2021-11-06 18:42:17 +0530

    Adding Start Pattern

---
+++ starPattern4.java
@@ -0,0 +1,32 @@
+class starPattern4{
+    public static void main(String[] args){
+       
+        // star pattern 3
+        
+        for(int i=1; i<=10; i++){
+            
+            for(int j = 10; j >= i; j--){
+                
+                System.out.print("*");
+
+            }
+           
+          System.out.println();
+        }
+    
+
+    // star pattern 4 
+    for(int i=1; i<=10; i++){
+            
+        for(int j = 10; j > i; j--){
+            
+            System.out.print(" ");
+
+        }
+        for(int k = 1;k<=i;k++){
+            System.out.print("*");
+        }
+      System.out.println();
+    }
+  }
+}
